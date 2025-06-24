b87b7ed13be5700056b8d842d125a80da83b7745
TingSter <ting@buzzwoo.de>
2025-06-24 17:20:53 +0700

    Reset data

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,11 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-// This is where we'll build our mini weather app
-const appName = ref<string>('Mini Weather App');
-const isLoading = ref<boolean>(false);
+<script>
+export default {
+  data() {},
+};
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <h1>{{ appName }}</h1>
-    </header>
-    <main>
-      <p>Welcome to your Vue.js weather application!</p>
-      <p>Ready to start building amazing features.</p>
-      <div v-if="isLoading">Loading weather data...</div>
-    </main>
-  </div>
+  <div id="app"></div>
 </template>
 
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-header h1 {
-  color: #42b983;
-  margin-bottom: 30px;
-}
-
-main {
-  margin: 0 auto;
-  max-width: 600px;
-  padding: 20px;
-}
-</style>
+<style scoped></style>
