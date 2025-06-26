61e73762a9a4ad2b3f7d7767ad1a2b6c3bc8fdc2
Aum <asdddaumm@gmail.com>
2025-06-26 16:10:47 +0700

    Create Component And Already Import Component To File 'App.vue' Kub.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import headerComponent from "./component/headerComponent.vue";
+import navbarComponent from "./component/navbarComponent.vue";
+import mainComponent from "./component/mainComponent.vue";
+
 export default {
-  data() {},
+  data() {
+    return {};
+  },
+  components: {
+    headerComponent,
+    navbarComponent,
+    mainComponent
+  }
 };
+
 </script>
 
 <template>
-  <div id="app"></div>
+  <div id="app" class="bg-blue-100 min-h-screen">
+    <header>
+      <headerComponent />
+      <navbarComponent />
+    </header>
+    <main class="max-w-5xl mx-auto py-6">
+      <mainComponent />
+      
+    </main>
+    <footer>
+      
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>
